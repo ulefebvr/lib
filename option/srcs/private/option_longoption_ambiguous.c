add5d36f629cfaa88fec3ae2da2adbfa91ee71d7
ulefebvr <ulefebvr@student.42.fr>
2016-12-25 01:47:10 +0100

    [OPTION] Fix memory leaks

--- option/srcs/private/option_longoption_ambiguous.c
+++ option/srcs/private/option_longoption_ambiguous.c
@@ -3,21 +3,22 @@
 #include "option_longoption.h"
 #include "option_tools.h"
 
+#include <stdlib.h>
+
 int __option_treat_longoption_ambiguous(char **argv, t_treat_longoption *info, t_option_data *d)
 {
-    struct option_list first;
+    t_option_list *tmp;
 
     if (d->opterr)
     {
-        first.p = info->pfound;
-        first.next = info->ambig_list;
-        info->ambig_list = &first;
-        ft_fdprint(2, "%s: option '%s' is ambiguous; possibilities:", 
-            argv[0], argv[d->optind]);
+        ft_fdprint(2, "%s: option '%s' is ambiguous; possibilities: '--%s'", 
+            argv[0], argv[d->optind], info->pfound->name);
         while (info->ambig_list != 0)
         {
             ft_fdprint(2, " '--%s'", info->ambig_list->p->name);
+            tmp = info->ambig_list;
             info->ambig_list = info->ambig_list->next;
+            free(tmp);
         }
         ft_fdprint(2, "\n");
     }
